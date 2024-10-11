41aeeb0499d86f9f0b7af6df55ca4e0a47b0a25c
arg3r3v <124920442+arg3r3v@users.noreply.github.com>
2024-10-10 20:33:07 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.getElementById('menu-button');
+    const fullscreenMenu = document.getElementById('fullscreen-menu');
+    const overlay = document.getElementById('overlay');
+
+    menuButton.addEventListener('click', () => {
+        fullscreenMenu.classList.toggle('open');
+        overlay.style.display = overlay.style.display === 'block' ? 'none' : 'block';
+    });
+
+    overlay.addEventListener('click', () => {
+        fullscreenMenu.classList.remove('open');
+        overlay.style.display = 'none';
+    });
+});
+    
+    window.onload = function() {
+        document.getElementById('popup').style.display = 'block';
+    };
+
+    function closePopup() {
+        document.getElementById('popup').style.display = 'none';
+    }
